f3ac2c7142839bfcb52c2123620c06caf81e94de
rdenck <ranlens1denck@gmail.com]>
2021-04-13 21:07:37 -0300

    calculadora simples em java

---
+++ src/operacoes/OperaçoesBasicas.java
@@ -0,0 +1,39 @@
+package Operacoes;
+
+public class OperaçoesBasicas {
+    // quando faz uso dessa class é criado um metodo ou parametro
+
+    // dentro do parenteses é o parametro.
+    public double somar(double a, double b) {
+
+        double resultado = a + b;
+        // aqui definir o retorno do metodo 
+        return resultado;
+    }
+
+    public double subtrair(double a, double b) {
+
+        double resultado = a - b;
+        return resultado;
+
+    }
+
+    public double multiplicar(double a, double b) {
+
+        double resultado = a * b;
+        return resultado;
+
+    }
+
+    public double dividir(double a, double b) {
+
+        double resultado = a / b;
+        return resultado;
+
+    }
+
+    public void somar(String display, String display0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
